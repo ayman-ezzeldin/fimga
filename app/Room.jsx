c89f507a9092b74648f350475ee6ac3474e70231
Ayman ezz <aymanmae12@gmail.com>
2024-08-17 16:21:30 +0300

    set up the env

---
+++ app/Room.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_IEAahZHkmhgi99X9B_-RbwgvDAIM-3TMmXYOgJU7qNgZjEkJflv1dqCX-udRXCkV"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
---
+++ components/Aym.tsx
@@ -0,0 +1,11 @@
+
+const Aym = () => {
+  return (
+    <>
+    <div className=" text-green-600 font-bold text-3xl" >Aym</div>
+    <div className=" text-green-600 font-bold text-3xl" >Aym</div>
+    </>
+  )
+}
+
+export default Aym
